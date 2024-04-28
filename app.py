6a05e69a31d0c33db31e557a27d3eaf17bc081d7
majid543 <134961299+majid543@users.noreply.github.com>
2024-04-27 21:25:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,6 @@ class QueryModel(BaseModel):
 @app.post("/")
 async def get_context(
     query_data: QueryModel,
-    credentials: HTTPAuthorizationCredentials = Depends(validate_token),
-):
     # convert query to embeddings
     res = openai_client.embeddings.create(
         input=[query_data.query], model="text-embedding-ada-002"
